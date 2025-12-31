fcedf5aa509b51ce4377d39b8506608aaf97dcfb
simon kuria <qushrickman@gmail.com>
2025-12-31 09:23:35 +0300

    addition of the details to my read file

--- billing/views.py
+++ billing/views.py
@@ -11,23 +11,23 @@ class PaymentViewSet(ModelViewSet):
     def get_queryset(self):
         user = self.request.user
 
-        # Landlord sees all payments
+        
         if user.profile.role == 'LANDLORD':
             return Payment.objects.all()
 
-        # Tenant sees only their own payments
+       
         if user.profile.role == 'TENANT':
             return Payment.objects.filter(rent__tenant__user=user)
 
         return Payment.objects.none()
 
     def perform_create(self, serializer):
-        # Tenant can create payment for their rent only
+       
         user = self.request.user
         if user.profile.role == 'TENANT':
             serializer.save()
         else:
-            # Optionally raise permission denied
+            
             serializer.save()
 
 class LateFeeViewSet(ModelViewSet):
